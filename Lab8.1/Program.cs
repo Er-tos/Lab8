e9f318f53c674dcd36be02078dad4399073e2128
Анатолий Ершов <a.v.ershov@sgugit.ru>
2021-11-23 22:52:52 +0700

    Добавьте файлы проекта.

---
+++ Lab8.1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Работа/NextGIS/DTClassifier";
+            string[] diresInParent;
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Содержимое папки, расположенной по дирректории: {0}", path);
+                diresInParent = GetDirsFromDir(path);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Данной директории не существует!");
+            }
+            Console.ReadKey();
+        }
+        static string[] GetDirsFromDir(string path)
+        {
+            string[] dires = new string[0];
+            string[] files = new string[0];
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("В директории {0} найдено:", path);
+                // Ищем папки в поданной директории
+                dires = Directory.GetDirectories(path);
+                if (dires.Length > 0)
+                {
+                    foreach (string dir in dires)
+                    {
+                        Console.WriteLine("     - найдена папка     {0}", dir);
+                    }
+                }
+                // Ищем файлы в поданной директории
+                files = Directory.GetFiles(path);
+                if (files.Length > 0)
+                {
+                    foreach (string file in files)
+                    {
+                        Console.WriteLine("     - найден файл     {0}", file);
+                    }
+                }
+                if (dires.Length > 0)
+                {
+                    foreach (string dir in dires)
+                    {
+                        GetDirsFromDir(dir);
+                    }
+                }
+            }
+            return dires;
+        }
+    }
+}
---
+++ Lab8.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToDir = "Example";
+            if (!Directory.Exists(pathToDir))
+            {
+                Directory.CreateDirectory(pathToDir);
+            }
+            string fileName = "NewFile.txt";
+            string pathToFile = pathToDir + "/" + fileName;
+            if (!File.Exists(pathToFile))
+            {
+                File.Create(pathToFile);
+            }
+            int num = 10;
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(pathToFile, false))
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    int intFromRandom = random.Next(0, 100);
+                    sw.WriteLine(intFromRandom);
+                }
+            }
+            int sum = 0;
+            using (StreamReader sr = new StreamReader(pathToFile))
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    int number = Convert.ToInt32(sr.ReadLine());
+                    sum += number;
+                    Console.WriteLine(number);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сумма чисел: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
